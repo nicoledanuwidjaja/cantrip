338220a44a1035cc83f0bed47063ea58af4ccbeb
nicoledanuwidjaja <danuwidjajanicole@gmail.com>
2019-07-14 23:21:48 -0400

    all files 7-14-19

---
+++ app/src/main/java/com/narwhalcompany/cantrip/DetailedTripActivity.java
@@ -0,0 +1,14 @@
+package com.narwhalcompany.cantrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_trip);
+    }
+}
