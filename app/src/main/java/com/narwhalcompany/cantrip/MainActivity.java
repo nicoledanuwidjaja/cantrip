e9d569686fc2df00422a451edf49ca229d0e6d63
sharonhe98 <36315984+sharonhe98@users.noreply.github.com>
2019-07-18 11:33:27 -0400

    [MODIFY] Re-add the new trip screen to button

--- app/src/main/java/com/narwhalcompany/cantrip/MainActivity.java
+++ app/src/main/java/com/narwhalcompany/cantrip/MainActivity.java
@@ -4,14 +4,18 @@ package com.narwhalcompany.cantrip;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView myPlans;
     TextView rec;
+
+    Button addNewTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
 
         myPlans = findViewById(R.id.MyPlans);
         rec = findViewById(R.id.Recommended);
+
+        addNewTrip = findViewById(R.id.add_trip_button);
+
+        addNewTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNewTripScreenActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Nicole: onClick Listeners for switching fragment views
 
