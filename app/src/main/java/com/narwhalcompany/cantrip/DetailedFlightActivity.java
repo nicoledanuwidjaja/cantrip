f3145896b702fe7aac4430c412e8e6e283b29042
Caroline Liu <caroline.liu111@gmail.com>
2019-07-18 02:08:22 -0400

    flight screen complete

---
+++ app/src/main/java/com/narwhalcompany/cantrip/DetailedFlightActivity.java
@@ -0,0 +1,14 @@
+package com.narwhalcompany.cantrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedFlightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_flight);
+    }
+}
