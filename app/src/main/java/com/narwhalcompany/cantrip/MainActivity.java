62bf6d191beeb3d0a33a6fa5f04f7f0beb33ece4
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-07-21 02:32:40 -0400

    TabLayout added and other visualizations tweaked 7-21-2019

--- app/src/main/java/com/narwhalcompany/cantrip/MainActivity.java
+++ app/src/main/java/com/narwhalcompany/cantrip/MainActivity.java
@@ -34,53 +34,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        // Nicole: onClick Listeners for switching fragment views
-
-//        myPlans.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-                // attach my fragment to my container that's provided in my layout
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                // add a function to your transaction
-                // @param - container id = FrameLayout from MAIN XML
-                // @param - which fragment should I add
-                transaction.add(R.id.fragmentContainer, new MyTripsFragment());
-
-                // Commit the changes
-                transaction.commit();
-//            }
-//        });
-
-        rec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // attach my fragment to my container that's provided in my layout
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                // add a function to your transaction
-                // @param - container id = FrameLayout from MAIN XML
-                // @param - which fragment should I add
-                transaction.replace(R.id.fragmentContainer, new EmptyRecommendedFragment());
-
-                // Commit the changes
-                transaction.commit();
-            }
-        });
-
-        myPlans.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // attach my fragment to my container that's provided in my layout
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                transaction.replace(R.id.fragmentContainer, new MyTripsFragment());
-
-                // Commit the changes
-                transaction.commit();
-            }
-        });
     }
 }
 
---
+++ app/src/main/java/com/narwhalcompany/cantrip/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,65 @@
+package com.narwhalcompany.cantrip.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.narwhalcompany.cantrip.EmptyRecommendedFragment;
+import com.narwhalcompany.cantrip.MyTripsFragment;
+import com.narwhalcompany.cantrip.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        Fragment selectedFragment = null;
+
+        switch (position) {
+            case 0:
+                selectedFragment = new MyTripsFragment();
+                break;
+            case 1:
+                selectedFragment = new EmptyRecommendedFragment();
+                break;
+        }
+        return selectedFragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "My Trips";
+            case 1:
+                return "Recommended";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+}
