da74b15980c37ce3fd73e1e9e79cf5dbe34c52c8
sharonhe98 <36315984+sharonhe98@users.noreply.github.com>
2019-07-30 17:11:24 -0400

    [ADD] Add new model objects
    - Add new model object for a trip called TripObject
    - Add new model object for a plan called ItineraryItem

---
+++ app/src/main/java/com/narwhalcompany/cantrip/model/main/TripObject.java
@@ -0,0 +1,13 @@
+package com.narwhalcompany.cantrip.model.main;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TripObject {
+
+    private Date startDate;
+    private Date endDate;
+    private String startLoc;
+    private String endLoc;
+    private ArrayList<ItineraryItem> plans;
+}
