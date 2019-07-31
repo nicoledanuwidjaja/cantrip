3ab9581c77b89acd42473f9818957489725aa673
Caroline Liu <caroline.liu111@gmail.com>
2019-07-31 19:50:10 -0400

    attractionFragment

--- app/src/main/java/com/narwhalcompany/cantrip/AttractionFragment.java
+++ app/src/main/java/com/narwhalcompany/cantrip/AttractionFragment.java
@@ -1,2 +1,29 @@
-public class AttractionFragment {
+package com.narwhalcompany.cantrip;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AttractionFragment extends AbstractPlanFragment {
+
+
+    public AttractionFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_attraction, container, false);
+
+
+        return view;
+    }
 }
+
