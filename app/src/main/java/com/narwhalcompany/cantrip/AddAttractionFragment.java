5a42d37988f7cb5dc74ccadf90706c8ede824b52
Caroline Liu <caroline.liu111@gmail.com>
2019-08-01 00:44:30 -0400

    addNewAttraction page

---
+++ app/src/main/java/com/narwhalcompany/cantrip/AddAttractionFragment.java
@@ -0,0 +1,50 @@
+package com.narwhalcompany.cantrip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddAttractionFragment extends DialogFragment {
+
+    private Button saveButton;
+
+
+    static AddAttractionFragment newInstance() {
+        return new AddAttractionFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogTheme);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_hotel, container, false);
+
+        saveButton = view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addHotelIntent = new Intent(getActivity(), DetailedTripActivity.class);
+                startActivity(addHotelIntent);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
