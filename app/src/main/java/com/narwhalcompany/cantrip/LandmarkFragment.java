41c1c42a0cbc088917a5ec07b1f463305a53077a
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-08-01 00:35:39 -0400

    api attempts

---
+++ app/src/main/java/com/narwhalcompany/cantrip/LandmarkFragment.java
@@ -0,0 +1,98 @@
+package com.narwhalcompany.cantrip;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.PhotoMetadata;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPhotoResponse;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LandmarkFragment extends AbstractPlanFragment {
+
+    private PlacesClient placesClient;
+
+    private ImageView attractionImage;
+
+    public LandmarkFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_landmark, container, false);
+
+        String apiKey = getString(R.string.google_maps_key);
+
+        attractionImage = view.findViewById(R.id.landmark_image);
+
+        List<Place.Field> fields = Arrays.asList(Place.Field.PHOTO_METADATAS);
+        FetchPlaceRequest placeRequest = FetchPlaceRequest.builder("ChIJ9U1mz_5YwokRosza1aAk0jM", fields).build();
+
+        // Initialize Places.
+        Places.initialize(getActivity().getApplicationContext(), apiKey);
+
+        // Create a new Places client instance.
+        placesClient = Places.createClient(getContext());
+
+        placesClient.fetchPlace(placeRequest).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
+            @Override
+            public void onSuccess(FetchPlaceResponse fetchPlaceResponse) {
+                Place place = fetchPlaceResponse.getPlace();
+
+                // Get the photo metadata.
+                PhotoMetadata photoMetadata = place.getPhotoMetadatas().get(0);
+
+                // Get the attribution text.
+                String attributions = photoMetadata.getAttributions();
+
+                // Create a FetchPhotoRequest.
+                FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(photoMetadata)
+                        .setMaxWidth(500) // Optional.
+                        .setMaxHeight(300) // Optional.
+                        .build();
+                placesClient.fetchPhoto(photoRequest).addOnSuccessListener(new OnSuccessListener<FetchPhotoResponse>() {
+                    @Override
+                    public void onSuccess(FetchPhotoResponse fetchPhotoResponse) {
+                        Bitmap bitmap = fetchPhotoResponse.getBitmap();
+                        attractionImage.setImageBitmap(bitmap);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ApiException) {
+                            ApiException apiException = (ApiException) e;
+                            int statusCode = apiException.getStatusCode();
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
+    }
+
+
+}
+
