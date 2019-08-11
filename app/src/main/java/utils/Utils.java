21784a28e36123c5d08c1ef0230654d464655029
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-08-10 20:23:23 -0400

    caught another one!

--- app/src/main/java/utils/Utils.java
+++ app/src/main/java/utils/Utils.java
@@ -60,8 +60,11 @@ public class Utils {
         String date = "";
         String startMonth;
 
-        if (datePicker.getMonth() < 10) {
-            startMonth = "0" + datePicker.getMonth();
+        if (datePicker.getMonth() < 9) {
+            int month = datePicker.getMonth() + 1;
+            startMonth = "0" + month;
+        } else if (datePicker.getMonth() == 9) {
+            startMonth = "10";
         } else {
             startMonth = "" + datePicker.getMonth() + 1 + "";
         }
